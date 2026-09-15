10dfce6d4873112b415f07e34d5fb4e170979937
Hannah Tang <hannahchuoi2015@gmail.com>
2026-09-15 18:09:24 -0500

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+  // Get the filename of the page currently open.
+  let currentPage = window.location.pathname.split("/").pop();
+
+  // GitHub Pages may open the homepage without showing index.html.
+  if (currentPage === "") {
+    currentPage = "index.html";
+  }
+
+  // Load the navigation information from menu.json.
+  $.getJSON("data/menu.json")
+    .done(function (data) {
+
+      // Begin creating the navigation.
+      let menuHTML = `
+        <nav class="main-menu">
+          <div class="website-name">
+            Alex's Portfolio
+          </div>
+
+          <div class="menu-links">
