58dac5f2baae77436f9a72fec7c2c76d1ef10168
Hannah Tang <hannahchuoi2015@gmail.com>
2026-09-15 18:46:29 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
       let menuHTML = `
         <nav class="main-menu">
           <div class="website-name">
-            Alex's Portfolio
+            Hannah's Portfolio
           </div>
 
           <div class="menu-links">
